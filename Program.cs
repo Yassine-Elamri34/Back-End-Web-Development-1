8e1043d68325d3b4d5c2b53677f4f68d8a705208
Yassine-Elamri34 <elamriyassin34@gmail.com>
2025-09-21 11:20:52 -0700

    Initial commit of C# project

---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿
+
+
+
+// lab 3
+// int year = 2025;
+// int yearOfBirth;
+
+// Console.WriteLine("Please enter your year of birth:");
+// yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+// if (yearOfBirth < 1000 || yearOfBirth > 2026)
+// {
+//     Console.WriteLine("Sorry! The year is invalid.");
+// }
+
+
+// if (yearOfBirth > 1000 || yearOfBirth < 2026)
+// {
+//     Console.WriteLine($"Your year of birth is {yearOfBirth}");
+// }
+// int age = year - yearOfBirth;
+// int password = 0000;
+// int yourPassword;
+// if (age >= 18)
+// {
+//     Console.WriteLine("please enter the password");
+//     yourPassword = Convert.ToInt32(Console.ReadLine());
+//     if (password != yourPassword)
+//     {
+//         Console.WriteLine("You are not allowed in the system");
+//     }
+//     else
+//     {
+//         Console.WriteLine("you are allowed into our system");
+//     };
+// }
+// else
+// {
+
+//     Console.WriteLine("Acces deniedd you are not allowed into our system !!!!");
+
+// }
+// ;
+
+
+
+
+
+
+
+// || hethi OR ou hethi && and 
+// int currentHealth = 10;
+// int maxHealth = 100;
+// int healingPotion = 1;
+// int potion;
+// Console.WriteLine("how many potion would you like to take");
+// potion = Convert.ToInt32(Console.ReadLine());
+// int CurHeal;
+// CurHeal = currentHealth * healingPotion;
+// Console.WriteLine("you used " + potion+ " potion and healed"+" " + CurHeal+ " HP!");
+// int after;
+// after = currentHealth + CurHeal;
+// Console.WriteLine("your current health is now :" + after);
+
+
+// int totalMinutes = 110;
+// int hours;
+// int minutes;
+// hours = totalMinutes / 60;
+// minutes = totalMinutes % 60;
+// minutes += 100;
+// Console.WriteLine(hours + " " + minutes);
+// int damage;
+// damage = Convert.ToInt32(Console.ReadLine());
+
+
+
+// int healthPower = 100;
+// int damage;
+// int healthLeft;
+// Console.WriteLine("Hey! , how much damage you want to do");
+// damage = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("the damage was : " + damage);
+// healthLeft = healthPower - damage;
+// Console.WriteLine("the monster health is now :" +  healthLeft);
+
+
+// int currentHealth = 10;
+// int maxHealth = 100;
+// int healingPotion = 1;
+// int potion;
+// Console.WriteLine("how many potion would you like to take");
+// potion = Convert.ToInt32(Console.ReadLine());
+// int CurHeal;
+// CurHeal = currentHealth * healingPotion;
+// Console.WriteLine("you used " + potion+ " potion and healed"+" " + CurHeal+ " HP!");
+// int after;
+// after = currentHealth + CurHeal;
+// Console.WriteLine("your current health is now :" + after);
+
+
+// int totalMinutes = 110;
+// int hours;
+// int minutes;
+
+// hours = totalMinutes / 60;
+// minutes = totalMinutes % 60;
+// `% HandleInheritability beki lkesma
+
+// Console.WriteLine(hours + " " + minutes);
+
+
+
+// string userInput;
+// int health;
+// userInput = Console.ReadLine();
+
+// health = Convert.ToInt32(userInput);
+// //hethi siiba chwaya 
+// Console.WriteLine(health);
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("input your health");
+// string userInput;
+//  userInput = Console.ReadLine();
+
+// Console.WriteLine("users health is ");
+// Console.WriteLine(userInput);
+
+
+
+
+
+
+
+
+// // mara lifetet
+// Console.WriteLine("type your name");
+// string userInput;
+//  userInput = Console.ReadLine();
+// Console.WriteLine("your name is ");
+// Console.WriteLine(userInput);
+// Console.WriteLine("type your Last name");
+// string userrInput;
+//  userrInput = Console.ReadLine();
+// Console.WriteLine("your Last name is ");
+// Console.WriteLine(userrInput);
+// string fullName = userInput + " " + userrInput;
+// Console.WriteLine("Your full name is: " + fullName);
+
+
+
+
+// Console.WriteLine("ikteb hakja");
+// string iktbHja;
+// iktbHja = Console.ReadLine();
+// Console.WriteLine("Your full name is :");
+// string userrrInput;
+//  userrrInput = Console.ReadLine();
+// Console.WriteLine("your Last name is ");
+// Console.WriteLine(userInput + userrInput);
+// int maxHealth = 100;
+// // float time = 1.5f;
+// string startingText = "Input your health";
+// tamel el output 
+//read input
+// el console.write tkhalina naaytou l hja yothhorli 
+// string userInput = Console.ReadLine();
+// Console.WriteLine("Input your health");
